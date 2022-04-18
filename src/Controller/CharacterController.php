4a0f7ce732b655af73002f70821cf8e00959238b
joeyschmitz <4plus7@gmail.com>
2022-04-18 14:08:01 +0200

    Some more styling.

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CharacterController extends AbstractController
+class CharacterController
 {
     /**
      * @var RickAndMortyApiClient
